244cd38b123fa0ceda530e5b242479611d1cb6d0
Alex Petrov <56254806+alexxpetrov@users.noreply.github.com>
2025-01-15 15:40:30 +0200

    enable streaming + rq query dehydration

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ import UserList from './components/SSRUsers/UserList';
 export default async function Home() {
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
+  queryClient.prefetchQuery({
     queryKey: ['users'],
     queryFn: authApi.getUsers,
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="m-auto grid w-1/2 grid-cols-2 p-8">
+      <div className="m-auto box-border grid w-2/3 grid-cols-2 gap-x-6 p-8">
         <UserList />
         <UserDetail />
       </div>
--- app/providers/ReactQueryProvider.tsx
+++ app/providers/ReactQueryProvider.tsx
@@ -2,6 +2,7 @@
 
 // Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
 import {
+  defaultShouldDehydrateQuery,
   isServer,
   QueryClient,
   QueryClientProvider,
@@ -21,6 +22,12 @@ function makeQueryClient() {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
         staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        // include pending queries in dehydration
+        shouldDehydrateQuery: query =>
+          defaultShouldDehydrateQuery(query)
+          || query.state.status === 'pending',
       },
     },
   });
